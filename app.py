5f860482053f6ecf0886dd2ab7a215dec3ad6746
ArtyomKlyuk <artyom010408@gmail.com>
2022-06-10 19:54:37 +0300

    Detector can't get dict

--- app.py
+++ app.py
@@ -1,43 +1,67 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import reqparse, abort, Api, Resource
 
 app = Flask(__name__)
-api = Api()
+api = Api(app)
 
-courses = {
-    1: {"name": "Python", "videos": 15},
-    2: {"name": "Java", "videos": 10}
-}
-
-
-class Pars:
-    parser = reqparse.RequestParser()
-    parser.add_argument("name", type=str)
-    parser.add_argument("videos", type=int)
-    parser
-
-class Main(Resource):
-    def get(self, course_id):
-        if course_id == 0:
-            return courses
-        else:
-            return courses[course_id]
-
-    def delete(self, course_id):
-        del courses[course_id]
-        return courses
-
-    def post(self, course_id):
-        courses[course_id] = Pars.parser.parse_args()
-        return courses
-
-    def put(self, course_id):
-        courses[course_id] = Pars.parser.parse_args()
-        return courses
+TODOS = {
+    'todo1': {'task': {
+        'num': '1234',
+        'test': 'eee'
+    }},
+    'todo2': {'task': {'num': '4321',
+                       'test': 'sss'},
+              'todo3': {'task': None},
+              }}
 
 
-api.add_resource(Main, "/api/courses/<int:course_id>")
-api.init_app(app)
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
 
-if __name__ == "__main__":
-    app.run(debug=True, port=3000, host="127.0.0.1")
+
+parser = reqparse.RequestParser()
+parser.add_argument('task', type=dict, location='form')
+
+
+# Todo
+# shows a single todo item and lets you delete a todo item
+class Todo(Resource):
+    def get(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
+
+    def put(self, todo_id):
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
+
+
+# TodoList
+# shows a list of all todos, and lets you POST to add new tasks
+class TodoList(Resource):
+    def get(self):
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
+
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(TodoList, '/todos')
+api.add_resource(Todo, '/todos/<todo_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
